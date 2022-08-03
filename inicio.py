8be6d0ce916a961f3297b5272b49e4df67210a14
Marcos Ariel Di Lauro <mdilauro39@gmail.com>
2022-08-03 18:34:12 -0300

    se completo la practica con los requerimientos pedidos

--- inicio.py
+++ inicio.py
@@ -1,13 +1,20 @@
+from ensurepip import bootstrap
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
 app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
 @app.route('/')
 def inicio():
-    return render_template('inicio.html',nombre="marcos")
+    nombre = "Marcos"
+    return render_template('inicio.html',nombre=nombre)
 
-@app.route('/nombre')
-def nombre():
-    return render_template()
+@app.route('/edad')
+def edad():
+    edad = 36
+    return render_template('edad.html', edad=edad)
 
 @app.route('/apellido')
 def apellido():
-    return render_template()
+    apellido = "DI LAURO"
+    return render_template('apellido.html', apellido=apellido)
